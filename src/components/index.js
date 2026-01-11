9bb27b6eef1b49e692eafdabb5175172d684d888
Sujal Bhagat <partimerbhindmask@gmail.com>
2026-01-11 09:22:11 -0800

    first commit

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-import Container from "./container/container";
+import Container from "./container/Container";
 import Footer from "./footer/Footer";
 import Header from "./header/Header";
 import LogoutBtn from "./header/LogoutBtn";
